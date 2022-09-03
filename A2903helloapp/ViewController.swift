d75dfbb56aa66503e07714dd00e3049c7f4ded98
Danny Shen <shenfive@gmail.com>
2022-09-03 17:36:54 +0800

    Fin

--- A2903helloapp/ViewController.swift
+++ A2903helloapp/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var numDispLabel: UILabel!
+    var lastNumber = 0
+    var needClear = false
+    
+    @IBAction func numberClick(_ sender: UIButton) {
+        if needClear {
+            numDispLabel.text = "0"
+            needClear = false
+        }
+        let string = sender.titleLabel?.text ?? ""
+        let orgString = numDispLabel.text ?? ""
+        let targetNumberString = orgString + string
+        let targetNumber = Int(targetNumberString) ?? 0
+        numDispLabel.text = "\(targetNumber)"
     }
-
-
+    @IBAction func clearAction(_ sender: Any) {
+        numDispLabel.text = "0"
+        lastNumber = 0
+    }
+    @IBAction func addAction(_ sender: Any) {
+        let currentNumber = Int(numDispLabel.text ?? "0") ?? 0
+        let ans = lastNumber + currentNumber
+        numDispLabel.text = "\(ans)"
+        lastNumber = ans
+        needClear = true
+    }
 }
 
